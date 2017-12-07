40c9d3cfa814f3dea77e8a6d29dafccc2b50c659
charles <charlesemery15@gmail.com>
2017-12-07 14:55:37 +0000

    Added sessions and moved assigning instance variables to gets

--- app.rb
+++ app.rb
@@ -1,21 +1,25 @@
 require 'sinatra/base'
-# set :session_secret, 'super secret'
 
 class Battle < Sinatra::Base
+  set :session_secret, 'crazy super duper secret'
+  enable :sessions
+
   get '/' do
     erb(:index)
     # 'Testing infrastructure working!'
   end
 
   post '/names' do
-    @player_one = params[:player_one]
-    @player_two = params[:player_two]
-    puts params
-    erb(:play)
+    session[:player_one] = params[:player_one]
+    session[:player_two] = params[:player_two]
+    redirect '/play'
   end
 
   get '/play' do
-    
+    @player_one = session[:player_one]
+    @player_two = session[:player_two]
+    p params
+    erb(:play)
   end
 
 
